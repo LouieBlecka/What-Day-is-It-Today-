1718af2462f44f05e39a1b06339b38917f5985d7
Louis M Blecka <lb73095@srcs.loc>
2020-02-24 10:44:14 -0800

    commit2

---
+++ Today.js
@@ -0,0 +1,20 @@
+function dayOfWeek(today) {
+    var date = new Date(today);
+    var weekday = date.getDay();
+    var day = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"];
+    var button = document.html.body.getElementById("dateButton");
+    var create = document.createElement("p");
+    var find = document.html.body.getElementById("todayIs");
+    find.create;
+    find.p.textContent = "day[weekday]";
+}
+button.addEventListener("click", () => {
+    dayOfWeek(document.html.body.getElementById("dateBox").value);
+})
