5f9f351c21fa65d02318ae01dc68fcafbbaf4d50
Aidan G Lubamersky <al51663@srcs.loc>
2020-03-04 13:30:30 -0800

    commit 6

--- Today.js
+++ Today.js
@@ -3,8 +3,7 @@ button.addEventListener("click", () => {
     dayOfWeek();
 });
 function dayOfWeek(today) {
-    today = document.body.getElementById("dateBox").value;
-    var date = new Date(today);
+    var date = new Date(document.body.getElementById("dateBox").value);
     var weekday = date.getDay();
     var day = [
     "Sunday",
@@ -15,6 +14,8 @@ function dayOfWeek(today) {
     "Friday",
     "Saturday"];
     var answer = day[weekday];
+    var create = document.createElement("p");
     var find = document.body.getElementById("todayIs");
-    document.find.appendChild(answer);
+    document.body.find.p.appendChild(answer);
+    document.find.appendChild(create);
 }
